3c7368e87a6b94a59e7700224dd8f101099e0ced
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-09-12 19:18:23 +0900

    Add files via upload

---
+++ Ex07.java
@@ -0,0 +1,12 @@
+package _9_6;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
