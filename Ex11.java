368f7d5fcec70c2d7531950175b306e4e60eabbb
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-09-15 13:37:58 +0900

    소수 판별

--- Ex11.java
+++ Ex11.java
@@ -1,5 +1,23 @@
 package _9_6;
 
-public class Ex11 {
+import java.util.Scanner;
 
+public class Ex09 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("철수: ");
+        String a = sc.next();
+        System.out.println("영희: ");
+        String b = sc.next();
+        
+        if (a.equals(b)) {
+            System.out.print("무승부");
+        }
+        else if(a.equals("s") && b.equals("p") || a.equals("r") && b.equals("s") || a.equals("p") && b.equals("r")){
+            System.out.print("철수, 승!");
+        }
+        else if(b.equals("s")&&a.equals("p") || b.equals("r")&&a.equals("s") || b.equals("p")&&a.equals("r")){
+            System.out.print("영희, 승!");
+        }
+    }
 }
