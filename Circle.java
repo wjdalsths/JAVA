6b02ca9ef72fe914b2e21e41026adae631b0d6f2
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-09-24 12:15:05 +0900

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,14 @@
+package Chap6;
+
+public class Circle {
+	private void secret() {
+		System.out.println("쉿 비밀임 ");
+	}
+	protected void findRadius() {
+		System.out.println("반지름이 10.0cm");
+		
+	}
+	public void findArea() {
+		System.out.println("넓이는 (π*반지름*반지름)이다");
+	}
+}
