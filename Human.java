1b5a024e182fe7bca8db366b22c7523e2615745b
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-09-25 22:55:12 +0900

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,13 @@
+package Chap04;
+
+public class Human {
+	String name;
+	double height;
+	
+	void dancing() {
+		System.out.println("키가 "+height+"인 "+name+"이 춤을 춘다");
+	}
+	void sing() {
+		System.out.println("키가 "+height+"인 "+name+"이 노래를 부른다");
+	}
+}
---
+++ HumanDemo.java
@@ -0,0 +1,18 @@
+package Chap04;
+
+public class HumanDemo {
+	public static void main(String[] args) {
+		Human j=new Human();
+		j.name ="jeongmin";
+		j.height=180;
+		j.dancing();
+		j.sing();
+		
+		Human sun=new Human();
+		sun.name="chehoon";
+		sun.height=160;
+		sun.dancing();
+		
+		
+	}
+}
