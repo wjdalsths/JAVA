7004c21802312192ec6b60a0291e2073d65d28cf
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-09-25 22:55:28 +0900

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,10 @@
+package Chap04;
+
+public class Phone {
+	String model;
+	int value;
+	
+	void print() {
+		System.out.println(value + "만원짜리 " + model + "스마트폰");
+	}
+}
---
+++ PhoneDemo.java
@@ -0,0 +1,16 @@
+package Chap04;
+
+public class PhoneDemo {
+	public static void main(String[] args) {
+		Phone sam=new Phone();
+		sam.model="samsung galax";
+		sam.value=100;
+		sam.print();
+		
+		Phone ip=new Phone();
+		ip.model="Apple";
+		ip.value=200;
+		ip.print();
+		
+	}
+}
