c490277c10a27232b4308c4d91713f64faa91999
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-10-28 20:46:32 +0900

    interface
    인터페이스 개념 기초

--- Ex03.java
+++ Ex03.java
@@ -1,7 +1,41 @@
-package _9_6;
+package Chap07;
 
-public class Ex03 {
-	public static void main(String[] args) {
-		
+interface Dinosaur {
+	public abstract void cry();
+	
+}
+class Trax implements Dinosaur{
+	public void cry() {
+		System.out.println("²Ù¾î¾î¾û");
+	}
+	public void walk() {
+		System.out.println("°È±â");
 	}
 }
+class Ptera implements Dinosaur{
+	public void cry() {
+		System.out.println("³¢¿¡¿¡");
+	}
+	public void fly() {
+		System.out.println("³¯±â");
+	}
+}
+public class Ex03 {
+	public static void main(String[] args) {
+		Trax t=new Trax();
+		Ptera p=new Ptera();
+		
+		Dinosaur[] d= {new Trax(),new Ptera()};
+		d[0].cry();
+		((Trax) d[0]).walk();
+		
+		d[0].cry();
+		((Ptera) d[1]).fly();
+		
+		
+//		t.cry();
+//		t.walk();
+//		p.cry();
+//		p.fly();
+	}
+}	
