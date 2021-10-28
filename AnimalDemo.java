a40cfa0779618c942c4410a3bbcb85895b555428
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-10-28 21:01:50 +0900

    interface
    인터페이스 다형성과 타입변환

---
+++ AnimalDemo.java
@@ -0,0 +1,30 @@
+package Presen;
+
+interface Animal{
+	void sound();
+}
+
+class Dog implements Animal{
+	public void sound() {
+		System.out.println("¸Û");
+	}
+}
+
+class Cuckoo implements Animal{
+	public void sound() {
+		System.out.println("»µ²Ú-");
+	}
+}
+
+public class AnimalDemo {
+	public static void main(String[] args) {
+		Dog d=new Dog();
+		Cuckoo c=new Cuckoo();
+		
+		makeSound(d);
+		makeSound(c);
+	}
+	static void makeSound(Animal a) {
+		a.sound();
+	}
+}
---
+++ MovableDemo.java
@@ -0,0 +1,33 @@
+package Presen;
+
+interface Movable {
+    void move(int x);
+}
+
+class Car implements Movable {
+	private int pos=0;
+	
+    public void move(int x) { //추상메서드
+        pos+=x;
+    }
+
+    public void show(){
+    	System.out.println(pos+"m 이동했습니다");
+    }
+
+	
+}
+
+public class MovableDemo {
+    public static void main(String[] args) {
+        Movable m = new Car();
+        
+        m.move(10);
+//        m.show(); 
+
+        Car c = (Car) m;
+        c.move(10);
+        c.show();
+    }
+}
+
