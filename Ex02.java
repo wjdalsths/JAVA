792db1c68775b9a59a984ffa4d9bba36953e9f50
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-10-28 20:44:41 +0900

    interface
    인터페이스 개념

--- Ex02.java
+++ Ex02.java
@@ -1,33 +1,38 @@
-package _9_6;
+package Chap07;
 
-import java.util.Scanner;
-
+interface Pay{
+	public void payment();
+	
+}
+class CardPay implements Pay{
+	public void payment() {
+		System.out.println("카드");
+	}
+}
+class CashPay implements Pay{
+	public void payment() {
+		System.out.println("현금");
+	}
+}
+class PaymentService{
+	Pay pay;
+	private final String Credit_card="카드결제";
+	private final String Cash ="현금결제";
+	
+	public void process(String op) {
+		if (op.equals(Credit_card)) {
+			pay=new CardPay();
+		}
+		else if(op.equals(Cash)) {
+			pay=new CashPay();
+		}
+		pay.payment();
+	}
+}
 public class Ex02 {
-	public static int factorial(int n) {
-		int re = 1;
-		while (n > 0) {
-			re *= n;
-			n -= 1;
-		}
-		return re;
-	}
-
-	public static int factorial(int n, int n1) {
-		int re = 1;
-		while (n <= n1) {
-			re *= n;
-			n += 1;
-		}
-		return re;
-	}
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int result = factorial(n);
-		System.out.println(result);
-		System.out.println(factorial(1, 5));
-		System.out.println(factorial(3, 5));
-		System.out.println(factorial(10, 5));
+		PaymentService a=new PaymentService();
+		a.process("카드결제");
+		a.process("현금결제");
 	}
 }
