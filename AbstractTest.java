2581186dfaaa5b3d345896f5c48e5c8ba932fba2
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-11-08 20:33:29 +0900

    추상 클래스
    Abstract class

---
+++ AbstractTest.java
@@ -0,0 +1,25 @@
+package _11_8;
+
+abstract class Abstract{
+    int i;
+    abstract void show();
+}
+class Concrete extends Abstract{
+    int j;
+
+    public Concrete(int i, int j) {
+        this.i=i;
+        this.j=j;
+    }
+    @Override
+    void show(){
+        System.out.println("i= "+i+", j= "+j);
+    }
+
+}
+public class AbstractTest{
+    public static void main(String[] args) {
+            Concrete c=new Concrete(100,50);
+            c.show();
+    }
+}
