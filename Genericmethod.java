aff1389c1906b16b67bb391402b9637b5ebeb054
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2021-11-11 22:53:45 +0900

    제네릭 메서드
    Generic Method

---
+++ Genericmethod.java
@@ -0,0 +1,30 @@
+package Chap10;
+
+import java.util.Vector;
+
+class Point{
+	private final int x,y;
+	public Point(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	public String toString() {
+		return "(" +x+","+y+")";
+	}
+}
+public class Genericmethod {
+	public static void main(String[] args) {
+		Vector<Point>v= new Vector<>();
+		
+		v.add(new Point(2,3));
+		v.add(new Point(-5,20));
+		v.add(new Point(30,-8));
+		
+		v.remove(1);
+		
+		for(int i=0;i<v.size();i++) {
+			Point p=v.get(i);
+			System.out.println(p);
+		}
+	}
+}
